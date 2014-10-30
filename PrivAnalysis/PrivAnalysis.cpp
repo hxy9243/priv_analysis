545a82d0a84e92b25ed21656fb94269c26bd96fe
Kevin Hu <hxy9243@gmail.com>
2014-10-30 00:16:02 -0400

    clean up and finish code layout in PrivAnalysis

--- PrivAnalysis/PrivAnalysis.cpp
+++ PrivAnalysis/PrivAnalysis.cpp
@@ -7,7 +7,10 @@
 //
 // ====-------------------------------------------------------====
 
+#include <stdio.h>
+
 #include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
@@ -15,19 +18,18 @@
 #include <linux/capability.h>
 
 #include <unordered_map>
+using namespace llvm;
 
 #define TARGET_FUNC "priv_lower"
 #define CAP_SIZE (CAP_LAST_CAP + 1)
 
-using namespace llvm;
-
 namespace {
   // Data structure for priv_lower capabilities in each function
   // Maps from InstCalls to -> Array of Capabilities
-  unordered_map <std::string, int[CAP_SIZE]>CAPTable;
+  std::unordered_map<std::string, int[CAP_SIZE]>CAPTable;
 
   // PrivAnalysis structure
-  struct PrivAnalysis : public FunctionPass {
+  struct PrivAnalysis : public ModulePass {
     static char ID;
     PrivAnalysis() : ModulePass (ID) {}
 
@@ -36,43 +38,61 @@ namespace {
       // Init data structure
       // TODO:
 
-
+      return false;
     }
 
     // Run on Module start
     virtual bool runOnModule (Module &M){
-      Function *F = getFunction (StringRef (TARGET_FUNC));
+      Function *F = M.getFunction (StringRef (TARGET_FUNC));
 
+      // protector
+      assert (F != NULL && "Cannot find target function!");
       // Find all users of function in the module
-      for (User *U : F->users()){
-	// If it's a call Inst calling the targeted function
-	if (CallInst *CI = dyn_cast<CallInst>(U) 
-	    && CI->getCalledFunction () == F){
+      for (User *U : F->users ()){
 
-	  // Retrieve all capabilities from params of function call
-	  // Note: Skip the first param of priv_lower for it's redundant
-	  unsigned numArgs = CI->getNumArgOperands ();
-	  for (int i = 1; i < numArgs; i ++){
-	    // retrieve integer value
-	    Value *v = CI->getArgOperand (i);
-	    APInt *vi = dyn_cast<ConstantInt>(v)->getValue();
-	    unsigned val = (unsigned) vi->getZExtValue ();
+      	// If it's a call Inst calling the targeted function
+	if (CallInst *CI = dyn_cast<CallInst>(U)){
+      	   if  (CI->getCalledFunction () == F) {
+
+      	    fprintf (stderr, "function is %p\n", F);
+      	    fprintf (stderr, "get function is %p\n", CI->getCalledFunction ());
+
+	    // Retrieve all capabilities from params of function call
+	    // Note: Skip the first param of priv_lower for it's num of args
+	    int numArgs = (int) CI->getNumArgOperands ();
+	    for (int i = 1; i < numArgs; i ++){
+	      // retrieve integer value
+	      Value *v = CI->getArgOperand (i);
+	      APInt *vi = dyn_cast<ConstantInt>(v)->getValue();
+	      unsigned val = (unsigned) vi->getZExtValue ();
+	      
+	      // Add it to the array
+
+	    }
+
+	    // Get the function where the Instr is in
+	    Function *tf = CI->getParent ()->getParent ();
+
+	    // Add to map
 
 
-	  }
 
-	  // Get the function where the Instr is in
-	  Function *tf = CI->getParent ()->getParent ();
-	  // Add to map
-
-
+	   } // if (CI->getCalledFunction() == F)
 	} // if (CallInst *CI = dyn_cast<CallInst>(U))
 
-      } // for (user *U : F->users ())
+      } // for ()
+
       return false;
     }  // virtual bool runOnModule
+
+
+    // preserve all analyses
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      AU.setPreservesAll();
+    }
+
   }; // endof struct PrivAnalysis
 }
 
 char PrivAnalysis::ID = 0;
-static RegisterPass<PrivAnalysis> X("PrivAnalysis", "Local privilege analysis pass in each function");
+static RegisterPass<PrivAnalysis> X("PrivAnalysis", "Local privilege analysis pass in each function", true, true);
