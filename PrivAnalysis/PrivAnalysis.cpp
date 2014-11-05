efee27e170913c44212fa0dcf33cbe4ed4059d02
Kevin Hu <hxy9243@gmail.com>
2014-11-05 00:25:38 -0500

    first time run successful PrivAnalysis in debug. finding all privs in one function

--- PrivAnalysis/PrivAnalysis.cpp
+++ PrivAnalysis/PrivAnalysis.cpp
@@ -15,22 +15,22 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include <linux/capability.h>
-#include <unordered_map>
-#include <vector>
+#include <map>
+#include <array>
 
 using namespace llvm;
 
 #define TARGET_FUNC "priv_raise"
-#define CAP_SIZE (CAP_LAST_CAP + 1)
+#define CAP_TOTALNUM (CAP_LAST_CAP + 1)
 
 namespace {
-  // Data structure for priv_lower capabilities in each function
-  // Maps from InstCalls to -> Array of Capabilities
-  std::unordered_map<*Function, std::vector<bool> >CAPTable;
-
   // PrivAnalysis structure
   struct PrivAnalysis : public ModulePass {
     static char ID;
+    // Data structure for priv_lower capabilities in each function
+    // Maps from InstCalls to -> Array of Capabilities
+    std::map<Function *, std::array<bool, CAP_TOTALNUM> >CAPTable;
+
     PrivAnalysis() : ModulePass(ID) {}
 
     // Do initialization
@@ -45,11 +45,13 @@ namespace {
     virtual bool runOnModule(Module &M){
       Function *F = M.getFunction(TARGET_FUNC);
 
-      // protector
-      // TODO: return instead of assert
-      // assert (F != NULL && "Cannot find target function!");
+      // Protector: didn't find any function TARGET_FUNC
+      if (F == NULL){
+        errs() << "Didn't find function " << TARGET_FUNC << "\n";
+        return false;
+      }
 
-      // Find all users of function in the module
+      // Find all user instructions of function in the module
       for (Value::user_iterator UI = F->user_begin(), UE = F->user_end(); UI != UE; ++UI){
         // If it's a call Inst calling the targeted function
         CallInst *CI = dyn_cast<CallInst>(*UI);
@@ -60,29 +62,50 @@ namespace {
         // Retrieve all capabilities from params of function call
         // Note: Skip the first param of priv_lower for it's num of args
         int numArgs = (int) CI->getNumArgOperands();
+        std::array<bool, CAP_TOTALNUM>CAParray = {0};
+
         for (int i = 1; i < numArgs; i ++){
           // retrieve integer value
           Value *v = CI->getArgOperand(i);
-          // problem here
           ConstantInt *I = dyn_cast<ConstantInt>(v);
           unsigned int iarg = I->getZExtValue();
 
-          errs() << "args is " << iarg << "\n";
-
           // Add it to the array
-          // TODO
-
+          CAParray[iarg] = 1;
         }
 
         // Get the function where the Instr is in
         Function *tf = CI->getParent()->getParent();
+        std::map<Function *, std::array<bool, CAP_TOTALNUM> >&pCAPTable 
+          = this->CAPTable;
+
+        // DEBUG
         errs() << "Parent of the instruction is " << tf->getName() << "\n";
 
-        // Add to map
-        // TODO
+        // If new, add to map, else, Union the two arrays
+        if (pCAPTable.find(tf) == pCAPTable.end() ){
+          pCAPTable[tf] = CAParray;
+        }
+        else {
+          for (int i = 0; i < CAP_TOTALNUM; ++ i){
+            pCAPTable[tf][i] |= CAParray[i];
+          }
+        }
 
-    
       } // for (Value::use_iterator
+
+      // DEBUG purpose: dump map table
+      // ---------------------//
+      for (auto mi = CAPTable.begin(), me = CAPTable.end(); mi != me; ++ mi){
+        errs() << mi->first->getName() << ":\n";
+        for (auto ai = mi->second.begin(), ae = mi->second.end(); ai != ae; ++ ai){
+
+          errs() << *ai << "\t";
+        }
+        errs() << "\n";
+      }
+      errs() << "\n";
+      // ----------------------//
 
       return false;
     }  // virtual bool runOnModule
