9e2ef86fe9811f8167d8bc24430ab620bc44d05d
Kevin Hu <hxy9243@gmail.com>
2014-11-05 10:26:35 -0500

    refactor code, use functions to make code more readable

--- PrivAnalysis/PrivAnalysis.cpp
+++ PrivAnalysis/PrivAnalysis.cpp
@@ -33,7 +33,9 @@ namespace {
 
     PrivAnalysis() : ModulePass(ID) {}
 
+    //
     // Do initialization
+    //
     virtual bool doInitialization(Module &M){
       // Init data structure
       // TODO:
@@ -41,42 +43,28 @@ namespace {
       return false;
     }
 
-    // Run on Module start
-    virtual bool runOnModule(Module &M){
-      Function *F = M.getFunction(TARGET_FUNC);
+    //
+    // Retrieve all capabilities from params of function call
+    //
+    void RetrieveAllCAP(CallInst *CI, std::array<bool, CAP_TOTALNUM> &CAParray){
+      int numArgs = (int) CI->getNumArgOperands();
 
-      // Protector: didn't find any function TARGET_FUNC
-      if (F == NULL){
-        errs() << "Didn't find function " << TARGET_FUNC << "\n";
-        return false;
+      for (int i = 1; i < numArgs; i ++){
+        // retrieve integer value
+        Value *v = CI->getArgOperand(i);
+        ConstantInt *I = dyn_cast<ConstantInt>(v);
+        unsigned int iarg = I->getZExtValue();
+
+        // Add it to the array
+        CAParray[iarg] = 1;
       }
+    }
 
-      // Find all user instructions of function in the module
-      for (Value::user_iterator UI = F->user_begin(), UE = F->user_end(); UI != UE; ++UI){
-        // If it's a call Inst calling the targeted function
-        CallInst *CI = dyn_cast<CallInst>(*UI);
-        if (CI == NULL || CI->getCalledFunction() != F){
-          continue;
-        }
-      
-        // Retrieve all capabilities from params of function call
-        // Note: Skip the first param of priv_lower for it's num of args
-        int numArgs = (int) CI->getNumArgOperands();
-        std::array<bool, CAP_TOTALNUM>CAParray = {0};
-
-        for (int i = 1; i < numArgs; i ++){
-          // retrieve integer value
-          Value *v = CI->getArgOperand(i);
-          ConstantInt *I = dyn_cast<ConstantInt>(v);
-          unsigned int iarg = I->getZExtValue();
-
-          // Add it to the array
-          CAParray[iarg] = 1;
-        }
-
-        // Get the function where the Instr is in
-        Function *tf = CI->getParent()->getParent();
-        std::map<Function *, std::array<bool, CAP_TOTALNUM> >&pCAPTable 
+    //
+    // Get the function where the CallInst is in, add to map
+    //
+    void AddFuncToMap(Function *tf, std::array<bool, CAP_TOTALNUM> CAParray){
+        std::map<Function *, std::array<bool, CAP_TOTALNUM> >&pCAPTable
           = this->CAPTable;
 
         // DEBUG
@@ -91,15 +79,51 @@ namespace {
             pCAPTable[tf][i] |= CAParray[i];
           }
         }
+    }
+
+    //
+    // Run on Module start
+    //
+    virtual bool runOnModule(Module &M){
+      Function *F = M.getFunction(TARGET_FUNC);
+
+      // Protector: didn't find any function TARGET_FUNC
+      if (F == NULL){
+        errs() << "Didn't find function " << TARGET_FUNC << "\n";
+        return false;
+      }
+
+      // Find all user instructions of function in the module
+      for (Value::user_iterator UI = F->user_begin(), UE = F->user_end();
+           UI != UE;
+           ++UI){
+        // If it's a call Inst calling the targeted function
+        CallInst *CI = dyn_cast<CallInst>(*UI);
+        if (CI == NULL || CI->getCalledFunction() != F){
+          continue;
+        }
+
+        // Retrieve all capabilities from params of function call
+        // Note: Skip the first param of priv_lower for it's num of args
+        std::array<bool, CAP_TOTALNUM>CAParray = {0};
+        RetrieveAllCAP (CI, CAParray);
+
+        // Get the function where the Instr is in
+        // Add CAP to Map (Function* => array of CAPs)
+        Function *tf = CI->getParent()->getParent();
+        AddFuncToMap(tf, CAParray);
 
       } // for (Value::use_iterator
 
       // DEBUG purpose: dump map table
       // ---------------------//
-      for (auto mi = CAPTable.begin(), me = CAPTable.end(); mi != me; ++ mi){
+      for (auto mi = CAPTable.begin(), me = CAPTable.end(); 
+           mi != me;
+           ++ mi){
         errs() << mi->first->getName() << ":\n";
-        for (auto ai = mi->second.begin(), ae = mi->second.end(); ai != ae; ++ ai){
-
+        for (auto ai = mi->second.begin(), ae = mi->second.end();
+             ai != ae;
+             ++ ai){
           errs() << *ai << "\t";
         }
         errs() << "\n";
@@ -110,8 +134,10 @@ namespace {
       return false;
     }  // virtual bool runOnModule
 
-
+    //
+    // getAnalysisUsage function
     // preserve all analyses
+    //
     void getAnalysisUsage(AnalysisUsage &AU) const override {
       AU.setPreservesAll();
     }
