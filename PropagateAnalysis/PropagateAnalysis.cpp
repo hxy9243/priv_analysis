7762abb3aeec52e8554b25649276d47a909142e2
Kevin Hu <hxy9243@gmail.com>
2014-11-06 01:43:01 -0500

    learning for call graph analysis, layout basic code structure for iterating through callgraphnodes

--- PropagateAnalysis/PropagateAnalysis.cpp
+++ PropagateAnalysis/PropagateAnalysis.cpp
@@ -8,58 +8,77 @@
  
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Analysis/CallGraph.h"
 #include "llvm/Analysis/CallGraphSCCPass.h"
-#include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <unordered_map>
+#include <array>
+#include <vector>
+#include <map>
 
 using namespace llvm;
-
-#define TARGET_FUNC "priv_lower"
-
-std::unordered_map<llvm::Function, std::array>CAPTable;
 
 namespace {
 
   // PrivAnalysis structure
   struct PropagateAnalysis : public CallGraphSCCPass {
     static char ID;
-    PropagateAnalysis () : CallGraphSCCPass (ID) {}
+    PropagateAnalysis() : CallGraphSCCPass(ID) {}
 
     // Do initialization
-    virtual bool doInitialization (CallGraph &CG){
+    virtual bool doInitialization(CallGraph &CG){
       // Init data structure
       // TODO:
 
 
+      // Iterate through the callgraph for callgraphnodes
+      for (CallGraph::iterator CI = CG.begin(), CE = CG.end();
+           CI != CE;
+           ++ CI){
 
+        const CallGraphNode *N = CI->second;
+        Function *FCaller = N->getFunction();
+        if (!FCaller){
+          continue;
+        }
+
+        // Iterate through each callgraphnode for callees
+        for (auto RI = N->begin(), RE = N->end(); RI != RE; ++ RI){
+          Function *FCallee = RI->second->getFunction();
+          if (!FCallee){
+            continue;
+          }
+
+          // DEBUG
+          //////////////
+          errs() << FCaller->getName()
+                 << " Calls function "
+                 << FCallee->getName() 
+                 << "\n";
+          //////////////
+        }
+      }
+
+      return false;
     }
 
-`    // Run on CallGraph SCC
-    virtual bool runOnSCC (CallGraphSCC &SCC){
+    // Run on CallGraph SCC
+    virtual bool runOnSCC(CallGraphSCC &SCC){
       
-      // Iterate over CallGraphNodes inside SCC
-      for (CallGraphNode *CGNI = SCC.begin (), *CGNE = SCC.end ();
-	   CGNI != CGNE;
-	   CGNI ++){
-
-	// Get the function 
-	Function *CalleeFunc = CGNI->getFunction ();
-
-	  // Iterate all Call Records inside CallGraphNodes 
-	for (CallRecord CRI = SCC.begin(), CRE = SCC.end ();
-	     CRI != CRE;
-	     CRI ++){
-
-	  // Get the callee of current call record
-	  CallGraphNode *CalleeNode = CRI.second;
-
-
-
-	}
-
-      }
+      // // Iterate over CallGraphNodes inside SCC
+      // for (CallGraphNode *CGNI = SCC.begin (), *CGNE = SCC.end ();
+      //      CGNI != CGNE;
+      //      CGNI ++){
+      //   // Get the function 
+      //   Function *CalleeFunc = CGNI->getFunction ();
+      //     // Iterate all Call Records inside CallGraphNodes 
+      //   for (CallRecord CRI = SCC.begin(), CRE = SCC.end ();
+      //        CRI != CRE;
+      //        CRI ++){
+      //     // Get the callee of current call record
+      //     CallGraphNode *CalleeNode = CRI.second;
+      //   }
+      // }
 
       return false;
     }
@@ -73,6 +92,6 @@ namespace {
   }; // endof struct PropagateAnalysis
 }
 
-char PrivAnalysis::ID = 0;
-static RegisterPass<PropagateAnalysis> X("PropagateAnalysis", "Value propagate analysis among call graph.");
+char PropagateAnalysis::ID = 0;
+static RegisterPass<PropagateAnalysis> X("PropagateAnalysis", "Privilege Propagate Analysis.");
 
