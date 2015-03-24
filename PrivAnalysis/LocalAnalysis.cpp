55fe75df6b052dd55a984c362347f7d2428462c4
Kevin Hu <hxy9243@gmail.com>
2015-03-24 00:05:08 -0400

    Asserting out the modules without priv_raise

--- PrivAnalysis/LocalAnalysis.cpp
+++ PrivAnalysis/LocalAnalysis.cpp
@@ -66,10 +66,7 @@ bool LocalAnalysis::runOnModule(Module &M)
     Function *F = M.getFunction(PRIVRAISE);
 
     // Protector: didn't find any function TARGET_FUNC
-    if (F == NULL){
-        errs() << "Didn't find function " << PRIVRAISE << "\n";
-        return false;
-    }
+    assert(F && "Didn't find function PRIV_RAISE function");
 
     // Find all user instructions of function in the module
     for (Value::user_iterator UI = F->user_begin(), UE = F->user_end();
